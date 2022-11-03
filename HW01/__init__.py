35d8e4a659d8378a5f11bd0b7ef2de5e7c717f0b
nimrod46 <nimrodma99@gmail.com>
2022-11-03 14:57:45 +0200

    Finished

--- HW01/__init__.py
+++ HW01/__init__.py
@@ -1,23 +1,28 @@
 import sys
 
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
 
 
 def start():
+    # matplotlib.use('TkAgg')
+
+    print("------Ex1:------")
+
     A = np.array([[1, 1, 0], [1, -2, 1], [2, 1, 2]])
     x = np.array([[1], [2], [-2]])
-    B = np.array([[1, -1, 1], [-1, 0, 1], [2, -1, 3]])
-    y = np.array([[2, -1, 3]])
+    B = np.array([[1, -1, 1], [-1, 0, 1], [3, 2, 1]])
+    y = np.array([2, -1, 3])
     C = np.array([[1, complex(0, -1)], [-1, complex(0, 1)], [0, complex(0, 2)]])
-    print()
-    print("Ax: " + str(A @ x))
-    # print("BxC: " + B @ x @ C)
+
+    print("Ax:\n" + str(A @ x))
     print()
     print("BxC: " + "Illegal!")
     print()
     print("yAB: " + str(y @ A @ B))
     print()
-    print("CC^tx: " + str(C @ C.transpose() @ x))
+    print("C(C^t)x:\n" + str(C @ C.transpose() @ x))
     print()
     print("yx: " + str(y @ x))
     print()
@@ -32,21 +37,47 @@ def start():
 
     print("sum2: " + str(sum))
 
-    u = np.array([complex(0, 2), 3, 5, -2, 5]).transpose()
-    v = np.array([complex(0, 1), complex(0, -1), 2, 1, 5]).transpose()
-    w = np.array([2, 3, complex(0, -3), -1, 3]).transpose()
-    print("<u, 2v>: " + str(innerProd(u, 2 * v)))
-    print("<u, v + 2w>: " + str(innerProd(u, v + 2 * w)))
+    print()
+    print("------Ex2:------")
+    print()
+
+    u = np.array([[complex(0, 2), 3, 5, -2, 5]]).transpose()
+    v = np.array([[complex(0, 1), complex(0, -1), 2, 1, 5]]).transpose()
+    w = np.array([[2, 3, complex(0, -3), -1, 3]]).transpose()
+
+    print("<u, 2v>: " + str(inner_prod(u, 2 * v)))
+    print("<u, v + 2w>: " + str(inner_prod(u, v + 2 * w)))
+
+    print()
+    print("------Ex3:------")
+    print()
+
+    fs = 44100
+    Ts = 1 / fs
+    x = np.arange(0, 1 - Ts, Ts)
+
+    y1 = np.cos(2 * np.pi * 1 * x)
+
+    y2 = np.sin(2 * np.pi * 1 * x)
+
+    plt.plot(x, y1, 'b', x, y2, 'r')
+
+    plt.show()
+
+    print("<y1, y2>(1): " + str(inner_prod(np.array([y1]).transpose(),
+                                           np.array([y2]).transpose())))
+
+    print("<y1, y2>(2): " + str((y1 * y2).sum()))
 
 
-    
-
-
-def innerProd(u, v):
-    if u.shape[0] != v.shape[0]:
-        print("Bad argumants: input is not a column vector!", file=sys.stderr)
+def inner_prod(u, v):
+    if u.shape[1] != 1 or v.shape[1] != 1:
+        print("Bad arguments: input is not a column vector!", file=sys.stderr)
         return 0
-    return np.inner(u, v)
+    if u.shape != v.shape:
+        print("Bad arguments: vector length does not match!", file=sys.stderr)
+        return 0
+    return np.vdot(u, v)
 
 
 if __name__ == '__main__':
