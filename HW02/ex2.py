86221d323b0f6f7fd917d0325ceaa266e9d804e6
nimrod46 <nimrodma99@gmail.com>
2022-11-20 19:52:51 +0200

    Fixed ex2 in HW2

--- HW02/ex2.py
+++ HW02/ex2.py
@@ -4,40 +4,34 @@ import sounddevice as sd
 import time
 
 
-def get_cos_array(min, max, f0):
-    fs = 10000
-    Ts = 1 / fs
-    t = np.arange(min, max, Ts)
+def get_cos_array(min_bound, max_bound, sample_freq, f0):
+    t = np.arange(min_bound, max_bound, sample_freq)
     x = np.cos(2 * np.pi * f0 * t)
-    return x
-    # sd.play(x, fs)
-    # plt.plot(t, x)
-    # plt.grid(axis='both')
-    # stitle = 'cosine function with freq = ' + str(f0)
-    # plt.title(stitle)
-    # plt.show()
-    # time.sleep(1)
+    return x, t
+
+
+def play_at_freq(freq):
+    fs = 10000
+    x, t = get_cos_array(0, 1, 1 / fs, freq)
+    print("Playing at: " + str(freq) + " freq")
+    sd.play(x, fs, blocking=True)
+    plt.plot(t[:100], x[:100])
+    plt.show()
 
 
 def cos_play500():
-    for i in range(500, 20000, 500):
-        fs = 10000
-        x = get_cos_array(0, 1, i)
-        sd.play(x, fs, blocking=True)
-        plt.plot(x)
-        plt.show()
+    for freq in range(500, 20500, 500):
+        play_at_freq(freq)
 
 
-# cos_play500()
+cos_play500()
 
 
-def cos_play_kromi():
+def cos_play_chromatic():
     freq = 440
-    while freq < 20000:
-        fs = 10000
-        x = get_cos_array(0, 1, freq)
-        sd.play(x, fs, blocking=True)
+    while freq <= 20000:
+        play_at_freq(freq)
         freq = freq * 2 ** (1 / 12)
 
 
-cos_play_kromi()
+cos_play_chromatic()
