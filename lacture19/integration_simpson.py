971264309b2a0263dfb44742e9b0542ee4a8f889
nimrod46 <nimrodma99@gmail.com>
2023-01-04 14:02:57 +0200

    Added lacture 19

---
+++ lacture19/integration_simpson.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def simpson_integration(a, b, func, n):
+    x = np.linspace(a, b, n)
+    f = func(x)
+    f[1:-1:2] *= 4
+    f[2:-2:2] *= 2
+    h = (b - a) / n
+
+    y = h / 3 * np.sum(f)
+    return y
+
+
+a = 1
+b = 3
+func = lambda x: 1 / x
+print(simpson_integration(a,b,func, 10000))
