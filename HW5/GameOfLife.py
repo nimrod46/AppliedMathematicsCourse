9bcb71182c4d3198024c34574d85b68ef2591824
nimrod46 <nimrodma99@gmail.com>
2023-01-14 16:59:05 +0200

    GameOfLife code improv

--- HW5/GameOfLife.py
+++ HW5/GameOfLife.py
@@ -8,9 +8,8 @@ def shifter(x, a, b):
     return np.roll(np.roll(x, a, axis=0), b, axis=1)
 
 
-def get_num_of_neighbours_at(mat):
-    biglive = np.zeros((mat.shape[0] + 2, mat.shape[1] + 2))
-    biglive[1:-1, 1:-1] = mat.astype(int)
+def get_num_of_neighbours_mat(mat):
+    biglive = mat.astype(int)
 
     live_count = shifter(biglive, -1, -1) + shifter(biglive, -1, 0) + shifter(biglive, -1, 1) + shifter(biglive, 0,
                                                                                                         -1) + shifter(
@@ -18,7 +17,6 @@ def get_num_of_neighbours_at(mat):
         0,
         1) + shifter(
         biglive, 1, -1) + shifter(biglive, 1, 0) + shifter(biglive, 1, 1)
-    live_count = live_count[1:-1, 1:-1]
 
     return live_count
 
@@ -28,8 +26,7 @@ def enforce_game_rule_on(src_mat, des_mat, dense_map, x, y, min, max):
     if src_mat[x, y] == 1:
         if num_of_neighbours < min or num_of_neighbours > max:
             des_mat[x, y] = 0
-        else:
-            des_mat[x, y] = 1
+
         return
 
     if num_of_neighbours == max:
@@ -48,7 +45,7 @@ def game_of_life(min_neighbours, max_neighbours, starting_dense):
 
         new_mat = mat.copy()
 
-        dense_map = get_num_of_neighbours_at(mat)
+        dense_map = get_num_of_neighbours_mat(mat)
         for i in range(n):
             for j in range(n):
                 enforce_game_rule_on(mat, new_mat, dense_map, i, j, min_neighbours, max_neighbours)
--- HW5/GameOfLifeBestVersion.py
+++ HW5/GameOfLifeBestVersion.py
@@ -9,13 +9,15 @@ def shifter(x, a, b):
     return np.roll(np.roll(x, a, axis=0), b, axis=1)
 
 
-def get_num_of_neighbours_at(mat):
-    biglive = np.zeros((mat.shape[0] + 2, mat.shape[1] + 2))
-    biglive[1:-1, 1:-1] = mat.astype(int)
+def get_num_of_neighbours_mat(mat):
+    biglive = mat.astype(int)
 
-    live_count = shifter(biglive, -1, -1) + shifter(biglive, -1, 0) + shifter(biglive, -1, 1) + shifter(biglive, 0, -1) \
-                 + shifter(biglive, 0, 1) + shifter(biglive, 1, -1) + shifter(biglive, 1, 0) + shifter(biglive, 1, 1)
-    live_count = live_count[1:-1, 1:-1]
+    live_count = shifter(biglive, -1, -1) + shifter(biglive, -1, 0) + shifter(biglive, -1, 1) + shifter(biglive, 0,
+                                                                                                        -1) + shifter(
+        biglive,
+        0,
+        1) + shifter(
+        biglive, 1, -1) + shifter(biglive, 1, 0) + shifter(biglive, 1, 1)
 
     return live_count
 
@@ -30,11 +32,11 @@ def game_of_life(min_neighbours, max_neighbours, starting_dense):
         plt.imshow(mat, cmap='gray')
         plt.show()
 
-        dense_map = get_num_of_neighbours_at(mat)
+        dense_map = get_num_of_neighbours_mat(mat)
         new_life = (mat == 0) & (dense_map == max_neighbours)
         end_life = (mat == 1) & ((dense_map > max_neighbours) | (dense_map < min_neighbours))
         mat[new_life] = 1
         mat[end_life] = 0
 
 
-game_of_life(2, 3, 0.5)
+game_of_life(2, 4, 0.08)
