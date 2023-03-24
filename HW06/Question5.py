6ccd6d1ea6ea5c949125f4a2a5e20a4a9b3b4d14
nimrod46 <nimrodma99@gmail.com>
2023-03-24 11:55:59 +0300

    Update Question5.py

--- HW06/Question5.py
+++ HW06/Question5.py
@@ -5,10 +5,6 @@ from matplotlib import pyplot as plt
 from HW06.Question3 import get_mean, get_variance, get_standard_deviation, get_coefficient_of_variation, get_median, \
     get_interquartile_range
 
-df = pd.read_csv('adult.csv', na_values=":", usecols=["hr_per_week", "sex",
-                                                      "age", 'income']).dropna()
-
-bar_names = ['mean', 'standard deviation', 'coefficient_of_variation', 'median', 'interquartile range']
 
 
 def analize(m_df, f_df, title):
@@ -25,10 +21,10 @@ def analize(m_df, f_df, title):
     f_coe_var = get_coefficient_of_variation(f_df)
     f_med = get_median(f_df)
     f_iqr = get_interquartile_range(f_df)
-    
+
     fig, ax = plt.subplots(1, 3, figsize=(16, 9))
 
-    print('---------------------')
+    print('-----------' + str(title) + '----------')
     print('Male:')
     print('Mean:' + str(m_mean))
     print('Standard Dev:' + str(m_standard_dev))
@@ -46,7 +42,7 @@ def analize(m_df, f_df, title):
     ax[0].set_title("Hours per week")
     ax[1].hist([m_df, f_df], bins=20, density=True, label=['Male', 'Female'])
     ax[1].set_title("Normalized Hours per week")
-    ax[2].hist([m_df, f_df], bins=20, cumulative=True, label=['Male', 'Female'])
+    ax[2].hist([m_df, f_df], bins=20, cumulative=True, density=True, label=['Male', 'Female'])
     ax[2].set_title("Cumulative Hours per week")
     ax[0].legend()
     ax[1].legend()
@@ -56,10 +52,31 @@ def analize(m_df, f_df, title):
 
 
 if __name__ == '__main__':
+    df = pd.read_csv('adult.csv', na_values=":", usecols=["hr_per_week", "sex",
+                                                          "age", 'income']).dropna()
+
+    bar_names = ['mean', 'standard deviation', 'coefficient_of_variation', 'median', 'interquartile range']
     m_df = df[df.sex.str.fullmatch('male', case=False)].hr_per_week
     f_df = df[df.sex.str.fullmatch('female', case=False)].hr_per_week
-    analize(m_df, f_df, 'Not filtered')
+    analize(m_df, f_df, 'Not filtered HPW')
 
-    m_df = df[df.sex.str.fullmatch('male', case=False) & df.income.str.fullmatch('>50K\n', case=False)].hr_per_week
-    f_df = df[df.sex.str.fullmatch('female', case=False) & df.income.str.fullmatch('>50K\n', case=False)].hr_per_week
-    analize(m_df, f_df, 'High income')
+    m_df_filtered = df[df.sex.str.fullmatch('male', case=False) &
+                       df.income.str.contains('>50K', case=False)].hr_per_week
+    f_df_filtered = df[df.sex.str.fullmatch('female', case=False) &
+                       df.income.str.contains('>50K', case=False)].hr_per_week
+    analize(m_df_filtered, f_df_filtered, 'High income HPW')
+
+    df2 = df.drop(df.index[(df.hr_per_week > df.hr_per_week.median() + 25) |
+                           (df.hr_per_week < df.hr_per_week.median() - 25)])
+
+    print("dropped: " + str(df.shape[0] - df2.shape[0]))
+
+    m_df = df2[df2.sex.str.fullmatch('male', case=False)].hr_per_week
+    f_df = df2[df2.sex.str.fullmatch('female', case=False)].hr_per_week
+    analize(m_df, f_df, 'Not filtered without outliers HPW')
+
+    m_df_filtered = df2[(df2.sex.str.fullmatch('male', case=False)) &
+                        (df2.income.str.contains('>50K', case=False))].hr_per_week
+    f_df_filtered = df2[(df2.sex.str.fullmatch('female', case=False)) &
+                        (df2.income.str.contains('>50K', case=False))].hr_per_week
+    analize(m_df_filtered, f_df_filtered, 'High income without outliers HPW')
